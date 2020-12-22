7f8a1e2d034f14e1e3d3a6b8b4711e5b714c40e1
melsimpson1023@gmail.com <L0v3ch1ld>
2020-12-22 11:37:11 -0500

    Changed approach to the chat
    Following different documentation for chatroom
    Therefore, had to make changes to the files.

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+from django.shortcuts import render
+
 def index(request):
-  return render(request, 'index.html', {})
+    return render(request, 'chat/index.html')
 
 def room(request, room_name):
-  return render(request, 'chatroom.html', {
-    'room_name': room_name
-  })
+    return render(request, 'chat/room.html', {
+        'room_name': room_name
+    })
--- project-sunshine/routing.py
+++ project-sunshine/routing.py
@@ -1,10 +1,16 @@
+from .wsgi import *
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import chat.routing
+import chat.routing as routing
 
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
-        URLRouter(
+        URLRouter([
+          re_path(r"^longpoll/$", LongPollConsumer.as_asgi()),
+          re_path(r"^notifications/(?P<stream>\w+)/$", LongPollConsumer.as_asgi()),
+          re_path(r"", get_asgi_application()),
+])
+        ]
             chat.routing.websocket_urlpatterns
         )
     ),
