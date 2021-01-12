9a1dfdf6fe47966f04958b8b76e1f7314ba7ba0b
melsimpson1023@gmail.com <L0v3ch1ld>
2021-01-12 11:09:39 -0500

    made changes to the crud request

--- curl-scripts/blog/delete.sh
+++ curl-scripts/blog/delete.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl "http://localhost:8000/blog/${ID}/" \
+curl "http://localhost:8000/blogs/${ID}/" \
   --include \
   --request DELETE \
   --header "Authorization: Token ${TOKEN}"
